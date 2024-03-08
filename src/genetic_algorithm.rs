6311284868f46ec68d8242afe2bf8ca382766f2c
FinnHuelsbusch <f.huelsbusch@outlook.de>
2024-03-08 11:01:25 +0100

    improve logging

--- src/genetic_algorithm.rs
+++ src/genetic_algorithm.rs
@@ -1,10 +1,5 @@
 use crate::{
-    config::Config,
-    crossover_functions::crossover,
-    mutation_functions::mutate,
-    population::{get_average_travel_time, initialize_population, Population},
-    problem_instance::ProblemInstance,
-    selection_functions::{parent_selection, survivor_selection},
+    config::Config, crossover_functions::crossover, individual::{self, Individual}, mutation_functions::mutate, population::{get_average_travel_time, initialize_population, Population}, problem_instance::ProblemInstance, selection_functions::{parent_selection, survivor_selection}
 };
 
 use std::io;
@@ -12,6 +7,8 @@ use std::io::Write;
 
 fn log_population_statistics(population: &Population) {
     let mut sorted_population: Population = population.clone();
+    // filter sorted_population to only include individuals with a feasible solution
+    sorted_population = sorted_population.iter().filter(|individual| individual.is_feasible()).cloned().collect();
     sorted_population.sort();
 
     println!(
@@ -24,6 +21,7 @@ fn log_population_statistics(population: &Population) {
 
 pub fn run_genetic_algorithm_instance(problem_instance: &ProblemInstance, config: &Config) {
     let mut population: Population = initialize_population(problem_instance, config);
+    let mut best_individual: Individual = population[0].clone();
 
     for generation in 0..config.number_of_generations {
         println!("Calculating Generation: {:?}", generation);
@@ -44,5 +42,10 @@ pub fn run_genetic_algorithm_instance(problem_instance: &ProblemInstance, config
         population = survivor_selection(&parents, &children, config);
 
         log_population_statistics(&population);
+        population.sort();
+        if population[0].travel_time < best_individual.travel_time {
+            best_individual = population[0].clone();
+        }
     }
+    println!("Best Individual: {:?}", best_individual);
 }
