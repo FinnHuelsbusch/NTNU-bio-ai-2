c6ab0ada824a361e3adafed6f339bcf07ce9062e
Felix Matthias Krumm <krumm-felix@web.de>
2024-03-09 13:19:44 +0100

    fix: empty elitism

--- src/selection_functions.rs
+++ src/selection_functions.rs
@@ -1,6 +1,6 @@
 use rand::Rng;
 
-use crate::{config::Config, population::Population};
+use crate::{ config::Config, population::Population };
 
 fn roulette_wheel_selection(population: &Population, population_size: usize) -> Population {
     let mut new_population: Population = Vec::with_capacity(population_size);
@@ -93,7 +93,11 @@ auto tournamentSelection(const Population &population, const FunctionParameters 
 }
  */
 
-fn tournament_selection(population: &Population, population_size: usize, config: &Config) -> Population {
+fn tournament_selection(
+    population: &Population,
+    population_size: usize,
+    config: &Config
+) -> Population {
     let tournament_size = config.parent_selection.tournament_size.unwrap();
     let tournament_probability = config.parent_selection.tournament_probability.unwrap();
     let mut new_population: Population = Vec::with_capacity(population_size);
@@ -110,11 +114,9 @@ fn tournament_selection(population: &Population, population_size: usize, config:
             .iter()
             .enumerate()
             .max_by(|(_, a), (_, b)| {
-                population[**a]
-                    .fitness
-                    .partial_cmp(&population[**b].fitness)
-                    .unwrap()
-            }).unwrap().0;
+                population[**a].fitness.partial_cmp(&population[**b].fitness).unwrap()
+            })
+            .unwrap().0;
 
         let rand_number = rng.gen::<f64>();
         if rand_number <= tournament_probability {
@@ -132,7 +134,7 @@ fn tournament_selection(population: &Population, population_size: usize, config:
 fn full_replacement_selection(
     population: &Population,
     children: &Population,
-    population_size: usize,
+    population_size: usize
 ) -> Population {
     assert_eq!(population.len(), population_size);
     assert_eq!(children.len(), population_size);
@@ -140,9 +142,9 @@ fn full_replacement_selection(
 }
 
 pub fn parent_selection(population: &Population, config: &Config) -> Population {
-    let number_of_elites = (config.parent_selection.elitism_percentage.unwrap_or(0.0)
-        * (config.population_size as f64))
-        .ceil() as usize;
+    let number_of_elites = (
+        config.parent_selection.elitism_percentage.unwrap_or(0.0) * (config.population_size as f64)
+    ).ceil() as usize;
     assert!(number_of_elites < config.population_size);
     let mut new_population: Population = Vec::with_capacity(config.population_size);
     if number_of_elites > 0 {
@@ -150,25 +152,27 @@ pub fn parent_selection(population: &Population, config: &Config) -> Population 
         elite_population.retain(|x| x.is_feasible());
         elite_population.sort_unstable_by(|a, b| b.fitness.partial_cmp(&a.fitness).unwrap());
 
-        // Add the elites to the new population
-        for i in 0..number_of_elites {
-            new_population.push(elite_population[i].clone());
+        if elite_population.len() >= number_of_elites {
+            // Add the elites to the new population
+            for i in 0..number_of_elites {
+                new_population.push(elite_population[i].clone());
+            }
         }
-        
     }
     let selected_population: Population = match config.parent_selection.name.as_str() {
         // Match a single value
         "rouletteWheel" => {
             roulette_wheel_selection(&population, config.population_size - number_of_elites)
-        },
+        }
         "tournament" => {
-            tournament_selection(&population,config.population_size - number_of_elites, config)
-        },
+            tournament_selection(&population, config.population_size - number_of_elites, config)
+        }
         // Handle the rest of cases
-        _ => panic!(
-            "Didn't have an Implementation for selection function: {:?}",
-            config.parent_selection.name.as_str()
-        ),
+        _ =>
+            panic!(
+                "Didn't have an Implementation for selection function: {:?}",
+                config.parent_selection.name.as_str()
+            ),
     };
     new_population.extend(selected_population);
     new_population
@@ -177,14 +181,10 @@ pub fn parent_selection(population: &Population, config: &Config) -> Population 
 pub fn survivor_selection(
     parents: &Population,
     children: &Population,
-    config: &Config,
+    config: &Config
 ) -> Population {
     let mut selection_population: Population;
-    if config
-        .survivor_selection
-        .combine_parents_and_offspring
-        .unwrap_or(false)
-    {
+    if config.survivor_selection.combine_parents_and_offspring.unwrap_or(false) {
         let mut combined_population: Population = parents.clone();
         combined_population.extend(children.clone());
         selection_population = combined_population;
@@ -194,35 +194,46 @@ pub fn survivor_selection(
     selection_population.sort_unstable_by(|a, b| b.fitness.partial_cmp(&a.fitness).unwrap());
 
     let mut new_population: Population = Vec::with_capacity(config.population_size);
-    let number_of_elites = (config.survivor_selection.elitism_percentage.unwrap_or(0.0)
-        * (config.population_size as f64))
-        .ceil() as usize;
+    let number_of_elites = (
+        config.survivor_selection.elitism_percentage.unwrap_or(0.0) *
+        (config.population_size as f64)
+    ).ceil() as usize;
     assert!(number_of_elites < config.population_size);
     if number_of_elites > 0 {
-        let mut  elite_population: Population  = selection_population.clone(); 
+        let mut elite_population: Population = selection_population.clone();
         elite_population.retain(|x| x.is_feasible());
         elite_population.sort_unstable_by(|a, b| b.fitness.partial_cmp(&a.fitness).unwrap());
-        for i in 0..number_of_elites {
-            new_population.push(elite_population[i].clone());
+
+        if elite_population.len() >= number_of_elites {
+            // Add the elites to the new population
+            for i in 0..number_of_elites {
+                new_population.push(elite_population[i].clone());
+            }
         }
     }
 
     let selected_population: Population = match config.parent_selection.name.as_str() {
         // Match a single value
-        "rouletteWheel" => roulette_wheel_selection(
-            &selection_population,
-            config.population_size - number_of_elites,
-        ),
+        "rouletteWheel" =>
+            roulette_wheel_selection(
+                &selection_population,
+                config.population_size - number_of_elites
+            ),
         "fullReplacement" => {
             full_replacement_selection(parents, children, config.population_size - number_of_elites)
-        },
+        }
         "tournament" => {
-            tournament_selection(&selection_population,config.population_size - number_of_elites, config)
-        },
-        _ => panic!(
-            "Didn't have an Implementation for selection function: {:?}",
-            config.parent_selection.name.as_str()
-        ),
+            tournament_selection(
+                &selection_population,
+                config.population_size - number_of_elites,
+                config
+            )
+        }
+        _ =>
+            panic!(
+                "Didn't have an Implementation for selection function: {:?}",
+                config.parent_selection.name.as_str()
+            ),
     };
     new_population.extend(selected_population);
     new_population
