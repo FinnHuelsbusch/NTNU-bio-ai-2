4d900f1606082d594b2bbb7a727064e72dd358b8
FinnHuelsbusch <f.huelsbusch@outlook.de>
2024-03-13 09:41:48 +0100

    improve build process

--- build_linux.sh
+++ build_linux.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 export MALLOC_CONF="thp:always,metadata_thp:always"
-cargo build --release
+RUSTFLAGS="-Cprofile-generate=/tmp/pgo-data" cargo build --release
+./target/release/bio-ai-2 ./configs/pgo_config.json     
+llvm-profdata merge -o /tmp/pgo-data/merged.profdata /tmp/pgo-data
+RUSTFLAGS="-Cprofile-use=/tmp/pgo-data/merged.profdata" cargo build --release
